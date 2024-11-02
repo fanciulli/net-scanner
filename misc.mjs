68914ab456eeddf69df0492078587e275a04269a
Massimiliano Fanciulli <fanciulli@gmail.com>
2024-11-02 10:00:30 +0100

    Adding header as resource file. Updating printHeader function to read header from file and print to console.log

--- misc.mjs
+++ misc.mjs
@@ -1,7 +1,8 @@
-function printHeader() {
-  console.log("*************************");
-  console.log("*    SCAN - NETWORK     *");
-  console.log("*************************");
+import { readFile } from "node:fs/promises";
+
+async function printHeader() {
+  const header = await readFile("./res/header.txt", "utf-8");
+  console.log(header);
 }
 
 export { printHeader };
