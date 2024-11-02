52f1aaeb587c812fa68b9abc4303fb1fa7d546b2
Massimiliano Fanciulli <fanciulli@gmail.com>
2024-11-02 10:01:02 +0100

    Moving iteration block to Scanner class

--- scanner.mjs
+++ scanner.mjs
@@ -1,5 +1,17 @@
+import { Netmask } from "netmask";
+
 export class Scanner {
   constructor(network) {
     this._network = network;
+  }
+
+  async scan() {
+    console.log(`Scanning network ${this._network}`);
+    console.log("");
+
+    var block = new Netmask(this._network);
+    block.forEach((ip, long, index) => {
+      console.log(`Scanning IP address ${ip}`);
+    });
   }
 }
