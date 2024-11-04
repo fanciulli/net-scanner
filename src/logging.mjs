9410e7da77326e55e9158b3b5b6897b0d638b26f
Massimiliano Fanciulli <fanciulli@gmail.com>
2024-11-04 20:54:16 +0100

    Adding log level.

--- src/logging.mjs
+++ src/logging.mjs
@@ -1,9 +1,9 @@
 function info(message) {
-  console.log(message);
+  console.log("[INFO] " + message);
 }
 
 function error(message) {
-  console.log(message);
+  console.log("[ERROR] " + message);
 }
 
 export { info, error };
