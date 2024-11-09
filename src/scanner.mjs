9e4100f959c91a483d807ffa42efd8ab1d5df07e
Massimiliano Fanciulli <fanciulli@gmail.com>
2024-11-09 17:28:25 +0100

    Updating scan to have a minimal scan with parseing of XML response and saving of data in JSON format.

--- src/scanner.mjs
+++ src/scanner.mjs
@@ -1,15 +1,47 @@
 import { Configuration } from "./configuration.mjs";
-import { info, error } from "./logging.mjs";
+import { info } from "./logging.mjs";
+import { parseStringPromise as xmlParse } from "xml2js";
+import { readFile, writeFile } from "node:fs/promises";
 import { promisify } from "node:util";
-import child_process from "node:child_process";
-const exec = promisify(child_process.exec);
+import { exec } from "node:child_process";
+const executeShell = promisify(exec);
 
 async function scanTarget(target) {
   info(`Scanning host ${target}`);
 
-  const { stdout, stderr } = await exec("nmap -A -oX scan.xml " + target);
-  info(stdout);
-  error(stderr);
+  await executeShell("nmap -sn -oX scan.xml " + target);
+}
+
+async function collectTargetInfo() {
+  const xml = await readFile("scan.xml");
+  const xmlAsObj = await xmlParse(xml);
+
+  const host = xmlAsObj.nmaprun.host[0];
+  const hostStatus = host.status[0].$.state;
+
+  const addresses = host.address;
+
+  const ipv4AddressSection = addresses.find(
+    (address) => address.$.addrtype == "ipv4"
+  );
+  const ipv4Address = ipv4AddressSection.$.addr;
+
+  const macAddressSection = addresses.find(
+    (address) => address.$.addrtype == "mac"
+  );
+  const macAddress = macAddressSection.$.addr;
+  const vendor = macAddressSection.$.vendor;
+
+  return {
+    status: hostStatus,
+    address: ipv4Address,
+    mac: macAddress,
+    vendor: vendor,
+  };
+}
+
+async function saveTargetInfo(targetInfo) {
+  await writeFile("targetInfo.json", JSON.stringify(targetInfo));
 }
 
 async function scan(configFile) {
@@ -18,6 +50,8 @@ async function scan(configFile) {
 
   if (config.target) {
     await scanTarget(config.target);
+    const targetInfo = await collectTargetInfo();
+    await saveTargetInfo(targetInfo);
   } else {
     throw Error("Nothing to do");
   }
