786afa33a7a732c715cdb75b12add371281e71d1
Massimiliano Fanciulli <fanciulli@gmail.com>
2024-11-12 04:30:02 +0100

    Updating coverage info

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,14 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { ecmaVersion: 'latest', globals: globals.node } },
+  { languageOptions: { ecmaVersion: "latest", globals: globals.node } },
   {
     rules: {
-      'no-duplicate-imports': 'error',
-      'no-template-curly-in-string': 'warn',
-      'no-use-before-define': 'error',
-      'require-atomic-updates': 'error'
-    }
+      "no-duplicate-imports": "error",
+      "no-template-curly-in-string": "warn",
+      "no-use-before-define": "error",
+      "require-atomic-updates": "error",
+    },
   },
-  pluginJs.configs.recommended
+  pluginJs.configs.recommended,
 ];
