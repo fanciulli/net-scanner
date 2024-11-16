1db621d3d2d83eff75c44c0469f69527341d0f3f
Massimiliano Fanciulli <fanciulli@gmail.com>
2024-11-16 08:52:39 +0100

    Adding pino logging library and Slack transport

--- src/logging.mjs
+++ src/logging.mjs
@@ -1,9 +1,49 @@
+import pino from "pino";
+
+let logger = undefined;
+
+function initLogger(configuration) {
+  const configLogger = configuration.logger;
+  if (configLogger) {
+    switch (configLogger.transport) {
+      case "console":
+        logger = pino();
+        break;
+
+      case "slack":
+        logger = pino({
+          transport: {
+            target: "@youngkiu/pino-slack-webhook",
+            level: configLogger.level,
+            options: {
+              webhookUrl: configLogger.options.webhookUrl,
+              channel: configLogger.options.channel,
+              username: configLogger.options.username,
+              icon_emoji: configLogger.options.icon_emoji,
+            },
+          },
+        });
+        break;
+    }
+  } else {
+    throw Error("Cannot find field logger in configuration");
+  }
+}
+
 function info(message) {
-  console.log("[INFO] " + message);
+  if (logger) {
+    logger.info(message);
+  } else {
+    console.log("[INFO] " + message);
+  }
 }
 
 function error(message) {
-  console.log("[ERROR] " + message);
+  if (logger) {
+    logger.error(message);
+  } else {
+    console.log("[ERROR] " + message);
+  }
 }
 
-export { info, error };
+export { initLogger, info, error, logger };
