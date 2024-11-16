7e1f3e018becdfc08ee48f9a53f8883849c2592e
Massimiliano Fanciulli <fanciulli@gmail.com>
2024-11-16 08:53:04 +0100

    Adding clarification line after application banner

--- src/misc.mjs
+++ src/misc.mjs
@@ -3,6 +3,10 @@ import { readFile } from "node:fs/promises";
 async function printHeader() {
   const header = await readFile("res/header.txt", "utf-8");
   console.log(header);
+  console.log();
+  console.log(
+    "Application output may be redirected to other transports. Please check configiuration file."
+  );
 }
 
 export { printHeader };
