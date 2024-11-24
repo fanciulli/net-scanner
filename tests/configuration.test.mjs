1a2d2c2d00873ff92a4609be4b475f740e076cd5
Massimiliano Fanciulli <fanciulli@gmail.com>
2024-11-24 19:44:58 +0100

    Enhancing UT to cover all cases in configuration

--- tests/configuration.test.mjs
+++ tests/configuration.test.mjs
@@ -15,6 +15,21 @@ test("throws an Error if configuration file cannot be accessed", async () => {
   }
 });
 
+test("throws an Error if configuration file is not a JSON", async () => {
+  expect.assertions(2);
+  try {
+    const testConfigData = "FILE = /bin/sh";
+
+    await writeFile(testConfigFilePath, testConfigData);
+    await config.load(testConfigFilePath);
+  } catch (error) {
+    expect(error.message).toContain("is not valid JSON");
+    expect(config.target).toBe(undefined);
+
+    await unlink(testConfigFilePath);
+  }
+});
+
 test("throws an Error if configuration file is invalid (1)", async () => {
   expect.assertions(2);
   try {
@@ -33,30 +48,52 @@ test("throws an Error if configuration file is invalid (1)", async () => {
 });
 
 test("reads configuration from file", async () => {
-  expect.assertions(1);
+  expect.assertions(3);
 
   await cp("./tests/resources/test-config-scan.json", testConfigFilePath);
   await config.load(testConfigFilePath);
 
   expect(config.target).toMatch("192.168.0.2");
-  expect(config.logger).toMatch(undefined);
+  expect(config.logger).toBeDefined();
+  expect(config.logger).toHaveProperty("transport", "console");
 
   await unlink(testConfigFilePath);
 });
 
-test("reads configuration from file with logger", async () => {
-  expect.assertions(1);
+test("reads configuration from file with Slack logger", async () => {
+  expect.assertions(8);
 
-  await cp(
-    "./tests/resources/test-config-scan-log-console.json",
-    testConfigFilePath
-  );
+  await cp("./tests/resources/test-config-scan-slack.json", testConfigFilePath);
   await config.load(testConfigFilePath);
 
   expect(config.target).toMatch("192.168.0.2");
-  expect(config.logger).toMatch({
-    transport: "console",
-  });
+  expect(config.logger).toBeDefined();
+  expect(config.logger).toHaveProperty("transport", "slack");
+  expect(config.logger).toHaveProperty("level", "info");
+  expect(config.logger).toHaveProperty(
+    "webhookUrl",
+    "https://hooks.slack.com/services/xxx/xxx/xxx"
+  );
+  expect(config.logger).toHaveProperty("channel", "#test-channel");
+  expect(config.logger).toHaveProperty("username", "webhookbot");
+  expect(config.logger).toHaveProperty("icon_emoji", ":ghost:");
+
+  await unlink(testConfigFilePath);
+});
+
+test("reads configuration from file without logger specified", async () => {
+  expect.assertions(3);
+
+  const testConfigData = {
+    target: "192.168.0.2",
+  };
+
+  await writeFile(testConfigFilePath, JSON.stringify(testConfigData));
+  await config.load(testConfigFilePath);
+
+  expect(config.target).toMatch("192.168.0.2");
+  expect(config.logger).toBeDefined();
+  expect(config.logger).toHaveProperty("transport", "console");
 
   await unlink(testConfigFilePath);
 });
