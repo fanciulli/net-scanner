17f5c3c257f58af7cb36b557f7ed184fdd7551e0
Massimiliano Fanciulli <fanciulli@gmail.com>
2025-01-05 11:24:34 +0100

    Updating logging.test.mjs

--- test/logging.test.mjs
+++ test/logging.test.mjs
@@ -1,46 +1,54 @@
-import { expect, test } from "@jest/globals";
+import { describe, test } from "mocha";
+import { should as shouldFun } from "chai";
+const should = shouldFun();
 import { Configuration } from "../src/configuration.mjs";
 import { copyConfigurationFile, deleteConfigurationFile } from "./utils.mjs";
 import { initLogger, logger, debug, info, error } from "../src/logging.mjs";
 
-test("Console logger initialization", () => {
-  expect(logger).toBeUndefined();
-  info("TEST");
-  error("TEST");
-  debug("TEST");
-});
+describe("Logging", () => {
+  describe("Console logger", () => {
+    test("Default initialization", () => {
+      should.equal(logger, undefined);
+      info("TEST");
+      error("TEST");
+      debug("TEST");
+    });
 
-test("Initialize Console logger", async () => {
-  await copyConfigurationFile(
-    "./tests/resources/test-config-scan.json",
-    "/tmp/config-console.json"
-  );
+    test("From configuration file", async () => {
+      await copyConfigurationFile(
+        "./test/resources/test-config-scan.json",
+        "/tmp/config-console.json"
+      );
 
-  const configuration = new Configuration();
-  await configuration.load("/tmp/config-console.json");
+      const configuration = new Configuration();
+      await configuration.load("/tmp/config-console.json");
 
-  initLogger(configuration);
+      initLogger(configuration);
 
-  expect(logger).toBeDefined();
-  info("TEST");
-  error("TEST");
-  debug("TEST");
+      logger.should.not.be.undefined;
+      info("TEST");
+      error("TEST");
+      debug("TEST");
 
-  await deleteConfigurationFile("/tmp/config-console.json");
-});
+      await deleteConfigurationFile("/tmp/config-console.json");
+    });
+  });
 
-test("Initialize Slack logger", async () => {
-  await copyConfigurationFile(
-    "./tests/resources/test-config-scan-slack.json",
-    "/tmp/config-slack.json"
-  );
+  describe("Slack transport", () => {
+    test("From configuration file", async () => {
+      await copyConfigurationFile(
+        "./test/resources/test-config-scan-slack.json",
+        "/tmp/config-slack.json"
+      );
 
-  const configuration = new Configuration();
-  await configuration.load("/tmp/config-slack.json");
+      const configuration = new Configuration();
+      await configuration.load("/tmp/config-slack.json");
 
-  initLogger(configuration);
+      initLogger(configuration);
 
-  expect(logger).toBeDefined();
+      logger.should.not.be.undefined;
 
-  await deleteConfigurationFile("/tmp/config-slack.json");
+      await deleteConfigurationFile("/tmp/config-slack.json");
+    });
+  });
 });
